f6e91e00b0f1a3b6e907dd28acf36ee8cb4ae06f
Phan Sĩ Thành <thanhphanscpy2005@gmail.com>
2026-04-08 08:23:14 +0700

    session with spring security

---
+++ SessionWithSpringSecurity/src/main/java/ithanh/ntu65133236/sess/SessionWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package ithanh.ntu65133236.sess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionWithSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ SessionWithSpringSecurity/src/test/java/ithanh/ntu65133236/sess/SessionWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ithanh.ntu65133236.sess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
