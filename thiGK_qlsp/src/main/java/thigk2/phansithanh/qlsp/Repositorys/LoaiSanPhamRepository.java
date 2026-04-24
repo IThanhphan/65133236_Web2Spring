6cdbce673cb76f6974cefd708b1aecdf32957511
Phan Sĩ Thành <thanhphanscpy2005@gmail.com>
2026-04-24 14:02:42 +0700

    add repository

---
+++ thiGK_qlsp/src/main/java/thigk2/phansithanh/qlsp/Repositorys/LoaiSanPhamRepository.java
@@ -0,0 +1,9 @@
+package thigk2.phansithanh.qlsp.Repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import thigk2.phansithanh.qlsp.Models.LoaiSanPham;
+
+public interface LoaiSanPhamRepository extends JpaRepository<LoaiSanPham, Integer> {
+
+}
---
+++ thiGK_qlsp/src/main/java/thigk2/phansithanh/qlsp/Repositorys/SanPhamRepository.java
@@ -0,0 +1,5 @@
+package thigk2.phansithanh.qlsp.Repositorys;
+
+public class SanPhamRepository {
+
+}
